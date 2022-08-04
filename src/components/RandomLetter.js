f99e03db22616eee5b6f62f488c5fd328709d67b
Nico <weiskopf.bionico@gmail.com>
2022-08-04 16:29:27 +0200

    Update RandomLetter.js

--- src/components/RandomLetter.js
+++ src/components/RandomLetter.js
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react"
 
 function RandomLetter () {
-    const [allLetters, setAllLetters] = useState("A")
     const lettersArray = [
         {id: 1, letter: "A"},
         {id: 2, letter: "B"},
@@ -30,31 +29,40 @@ function RandomLetter () {
         {id: 25, letter: "Y"},
         {id: 26, letter: "Z"},
     ]
-    //console.log(lettersArray)
     const getAllLetters = lettersArray.map((letterObj) => {return (letterObj.letter)})
+    const stringLetters = getAllLetters.toString()
+    // console.log(getAllLetters.toString())
+
+    const [allLetters, setAllLetters] = useState(stringLetters.replaceAll(',', ''))
+
+    /* the useState needs an original value of the whole alphabet,
+    so it can render a single random letter right from the start. 
+    if the default value is "", it will render single random Letters only,
+    when they were "added" to the useState the second time */
 
     let randomIndex = [Math.floor(Math.random()*getAllLetters.length)];
     let oneLetter = () => getAllLetters[randomIndex]
-    console.log(oneLetter())
+    //console.log(oneLetter())
 
     useEffect(() => {
         const interval = setInterval(() => {
-            setAllLetters((prevLetter) => prevLetter + 1)
+            setAllLetters((prevLetter) => prevLetter + oneLetter())
         }, 50);
         return() => clearInterval(interval);
-    }, [])
+    }, [allLetters])
+    console.log(allLetters)
     
 
     return (
         <div>
             {oneLetter()}
             <hr></hr>
+            {allLetters[randomIndex]} 
         </div>
     )
 }
 
 export default RandomLetter
 
-/* let randomIndex = [Math.floor(Math.random()*LettersArray.length)];
-    let oneLetter = LettersArray[randomIndex].letter
-    console.log(oneLetter) */
+/* let randomIndex = [Math.floor(Math.random()*getAllLetters.length)];
+    let oneLetter = () => getAllLetters[randomIndex] */
